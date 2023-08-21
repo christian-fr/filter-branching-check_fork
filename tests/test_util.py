8334b2992a16e9ca2d57fc51b33a694e664023d4
Christian Friedrich <mail-git@chr-fr.net>
2023-08-21 02:12:28 +0200

    updated

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,6 +1,8 @@
 from unittest import TestCase
-from context.graphs import get_consistent_graph_03
-from fbc.util import bfs_nodes
+from context.graphs import get_consistent_graph_03, get_consistent_graph_01
+from fbc.util import bfs_nodes, get_mask
+from fbc.eval import get_symbols
+from tests.context.util import mask_2, mask_5, mask_6
 
 
 class Test(TestCase):
@@ -8,3 +10,9 @@ class Test(TestCase):
         g, p1 = get_consistent_graph_03()
         n = bfs_nodes(g, 1)
         self.fail()
+
+    def test_get_mask(self):
+        self.assertEqual(get_mask(2), mask_2)
+        self.assertEqual(get_mask(5), mask_5)
+        self.assertEqual(get_mask(6), mask_6)
+
